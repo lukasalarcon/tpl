99e1473ad16047916916e3fba7a550a7ba339a62
lukasalarcon <lukas.alarcon@gmail.com>
2016-02-18 17:35:30 -0300

    K

--- setup_Splunk.sh
+++ setup_Splunk.sh
@@ -26,7 +26,10 @@ case $num in
 		if [ -f $splunkFol ]
 			then
 				echo "Package Found"							
-				SPLUNKPN=$splunkFol
+			SPLUNKPN=$splunkFol
+			#CALL THE INSTALLER
+			InstallSplunk
+
 			else
 
 				echo "Package not found. Exiting"
@@ -50,9 +53,51 @@ esac
 #End GetSplunk
 }
 
+
+
+
+function Parameters () {
+# check all parameters for splunk
+splbin=$(find / -name splunk -type f -perm -u+x)
+
+        if [ -f $splbin ]
+                then
+                        echo "Splunk Binary Detected"
+                        $splbin start --accept-license
+                else
+                        echo "Splunk Binary Not detected"
+                        $splbin enable boot-start
+
+        fi
+
+#firewall rule
+
+sudo firewall-cmd --zone=public --add-port=8000/tcp --permanent
+
+
+#function Parameters ends
+}
+
+
+
+function JoinFunctions () {
+#UNIFY 2 functions only
+
+                Parameters
+
+
+
+
+#end JoinFunctions
+}
+
+
 function InstallSplunk () {
 
-while [	$myop != "3"	]
+
+myop=0
+
+while [	"$myop" != 3 ]; do
 
 echo "Please, choose the following options"
 echo "1) Install in a specific folder"
@@ -60,35 +105,37 @@ echo "2) Install in the default folder"
 echo "3) Exit Install Process"
 read myop
 
-case "$myop" in 
+	case "$myop" in 
 
-	1) 
-		echo "Please, enter the folder where you want to install:"
-		read myfolder
-	   	echo "You have choose the following folder: $myfolder. Is this correct(y/n)?"
-		read ans
-		if [  "$ans" == "y" -o "$ans" == "Y" ]
-			then
-				echo "New Folder: $myfolder "
-				sudo chmod 744 $SPLUNKPN
-				sudo rpm -i --prefix=$myfolder $SPLUNKPN
-				JoinFunctions
-				myop=3
-		fi
+		1) 
+			echo "Please, enter the folder where you want to install:"
+			read myfolder
+	   		echo "You have choose the following folder: $myfolder. Is this correct(y/n)?"
+			read ans
+			if [  "$ans" == "y" -o "$ans" == "Y" ]
+				then
+					echo "New Folder: $myfolder "
+					sudo chmod 744 $SPLUNKPN
+					sudo rpm -i --prefix=$myfolder $SPLUNKPN
+					JoinFunctions
+					myop=3
+			fi
 		
-	;;
-	2)	 		echo "Default Folder"
-                                sudo chmod 744 $SPLUNKPN
-                                rpm -i $SPLUNKPN
-				JoinFunctions
-				myop=3
-	;;
-	3)	echo "Exiting..."
-		exit 1
-	;;
+			;;
+		2)	 		echo "Default Folder"
+                                	sudo chmod 744 $SPLUNKPN
+                                	rpm -i $SPLUNKPN
+					JoinFunctions
+					myop=3
+			;;
+		3)	echo "Exiting..."
+			exit 1
+			;;
 
 
-esac	
+		esac	
+done
+
 #installsplunk ends
 }
 
@@ -100,9 +147,10 @@ splbin=$(find / -name splunk -type f -perm -u+x)
 		then
 			echo "Splunk Binary Detected"
 			$splbin start --accept-license
+			$splbin enable boot-start
+
 		else
 			echo "Splunk Binary Not detected"
-			$splbin enable boot-start
 
 	fi
 
@@ -114,18 +162,6 @@ sudo firewall-cmd --zone=public --add-port=8000/tcp --permanent
 #function Parameters ends
 }
 
-function JoinFunctions () {
-#UNIFY 2 functions only
-
-	InstallSplunk
-		Parameters
-
-
-
-
-
-}
 
 #MAIN 
 	GetSplunk
-
