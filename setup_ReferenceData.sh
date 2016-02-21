2c4d664302edeabcefa86874afd76581e866b996
lukasalarcon <lukas.alarcon@gmail.com>
2016-02-21 18:59:40 -0500

    Y

--- setup_ReferenceData.sh
+++ setup_ReferenceData.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # GNU GPL Software under the GPL may be run for all purposes, including commercial purposes and even as a tool for creating proprietary software.
 
-
-
 #DEBUG PROCESS
 set -x
 #END DEBUG
@@ -14,13 +12,13 @@ set -x
 #END GLOBAL VARS
 
 
-function CentOS7_Install (){
+function CentOS7_Install () {
 #start CentOS7_Install
 
 
 #find if packages are already installed
-_apache=$(rpm -a httpd)
-_mysqlserver=$(rpm -a mariadb-server)
+_apache=$(rpm -qa httpd)
+_mysqlserver=$(rpm -qa mariadb-server)
 _php=$(rpm -qa php)
 _phpsql=$(rpm -qa php-mysql)
 #YUM?
@@ -28,7 +26,7 @@ _phpsql=$(rpm -qa php-mysql)
 #APACHE
 if [[ "$_apache" == "httpd"* ]]
         then
-                echo "Package APACHE              [OK]"
+                echo "Package APACHE		              [OK]"
         else
                 echo "Proceed to Install APACHE"
                 sudo yum -y install httpd
@@ -42,13 +40,10 @@ if [[ "$_mysqlserver" == "mariadb-server"* ]]
         then
                 echo "Package MARIADB SERVER              [OK]"
         else
-                echo "Proceed to Install APACHE"
+                echo "Proceed to Install MARIADB SERVER"
                 sudo yum -y install mariadb-server
-
-
 fi
 
-}
 
 #PHP
 if [[ "$_php" == "php"* ]]
@@ -63,14 +58,12 @@ fi
 
 
 #PHMYSQL
-if [[ "$_phpsql" == "php-sql"* ]]
+if [[ "$_phpsql" == "php-mysql"* ]]
         then
                 echo "Package PHP-SQL              [OK]"
         else
-                echo "Proceed to Install APACHE"
-                sudo yum -y install php-sql 
-
-
+                echo "Proceed to Install PHP-SQL"
+                sudo yum -y install php-mysql 
 fi
 
 
@@ -82,22 +75,19 @@ function StartServices () {
 
 
 #ENABLE MARIADB
-sudo systemctl enable mariadb.service
+sudo systemctl enable mariadb.service
 
 #ENABLE HTTP APACHE SERVICE
-sudo systemctl enable httpd.service
+sudo systemctl enable httpd.service
 
 #START MARIADB
-sudo systemctl start mariadb.service
+sudo systemctl start mariadb.service
 
 #START APACHE SERVICE
-sudo systemctl start httpd.service
+sudo systemctl start httpd.service
 
 #START MYSQL SERVICES
-sudo mysql_secure_installation
-
-
-
+sudo mysql_secure_installation
 
 }
 
@@ -110,20 +100,24 @@ function DataBaseSetting () {
 		then
 			echo "Found MySQL/MAriaDB"
 			#CAPTURE TOTAL MEMORY
-			TOMEM=$(free | head -2|cut -c10-20 | tail -1)
-			
+				TOMEM=$(free | head -2|cut -c10-20 | tail -1)
+					#CALCULATE 75%
+					PERC=$(( $TOMEM * 75 / 100))
+						#INSERT LINES FOR MEMORY MANAGEMENT
+							sed  '/\[mysqld\]/a max_allowed_packet = 16M'
+							sed  '/\[mysqld\]/a innodb_buffer_pool_size = ${PERC}G'
+							sed  '/\[mysqld\]/a innodb_file_per_table   = 1'
+						#SYSTEM RESTART
+						sudo systemctl restart mariadb.service				
 			
 		else
 			echo "we cannot find MySQL/MariaDB"
+			exit 1
 	fi
-
-
-
-
-
-
-
-
 }
 
+#MAIN
 
+	CentOS7_Install 
+		StartServices
+		DataBaseSetting
